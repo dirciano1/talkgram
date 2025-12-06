fe964ac81059bc10b3f11ff228542c1c456c6f1b
dirciano1 <51066028+dirciano1@users.noreply.github.com>
2025-12-06 15:14:56 -0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,6 +10,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+// Evita inicializar 2x no Next
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
